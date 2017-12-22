b8b8c9e732fcc5403409402647de0178aa6d7b9d
Linton Ye <linton@jimulabs.com>
2017-12-22 00:20:34 -0800

    Directly use Web Audio API to support iOS

--- src/Sound.js
+++ src/Sound.js
@@ -1,33 +1,79 @@
 import React from 'react';
-import Pizzicato from 'pizzicato';
+
+let audioContext = null;
+
+try {
+  window.AudioContext = window.AudioContext || window.webkitAudioContext;
+  audioContext = new AudioContext();
+} catch (e) {
+  console.log('Web Audio API is not supported in this browser', e);
+}
+
+function load(url) {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+    request.open('GET', url, true);
+    request.responseType = 'arraybuffer';
+    request.onload = () => {
+      audioContext.decodeAudioData(
+        request.response,
+        (buffer) => {
+          resolve(buffer);
+        },
+        () => { reject(`Load error: ${url}`); }
+      )
+    };
+    request.send();
+  });
+}
 
 class Sound extends React.Component {
-  componentDidMount() {
-    this._loadAndPlay(this.props);
+  _playEmptySound = () => {
+    // create empty buffer
+    var buffer = audioContext.createBuffer(1, 1, 22050);
+    var source = audioContext.createBufferSource();
+    source.buffer = buffer;
+    console.log('source', source);
+    // connect to output (your speakers)
+    source.connect(audioContext.destination);
+    // play the file
+    source.start(0);
   }
-  _loadAndPlay = (props) => {
-    this.sound = new Pizzicato.Sound({
-      source: 'file',
-      options: {
-        path: props.url,
-        loop: props.loop || false,
-      }
-    }, () => this.sound.play());     
+  _unlockIOSSound() {
+    if (audioContext) {
+      window.addEventListener('touchend', this._playEmptySound, false);
+    }
+  }
+  async componentDidMount() {
+    this._unlockIOSSound();
+    await this._loadAndPlay(this.props);
+  }
+  _loadAndPlay = async (props) => {
+    if (audioContext) {
+      const buffer = await load(props.url);
+      this.soundSource = audioContext.createBufferSource();
+      this.soundSource.buffer = buffer;
+      this.soundSource.connect(audioContext.destination);
+      this.soundSource.loop = props.loop || false;
+      this.soundSource.start(0);
+    }
   }
   _stopSound = () => {
-    if (this.sound) {
-      this.sound.pause();
-      this.sound.stop();
+    if (this.soundSource) {
+      this.soundSource.stop();
     }
   };
-  componentWillReceiveProps(nextProps) {
+  async componentWillReceiveProps(nextProps) {
     if (this.props.url !== nextProps.url) {
       this._stopSound();
-      this._loadAndPlay(nextProps);
+      await this._loadAndPlay(nextProps);
     }
   }
   componentWillUnmount() {
     this._stopSound();
+    if (audioContext) {
+      window.removeEventListener('touchend', this._playEmptySound);
+    }
   }
   render() {
     return null;

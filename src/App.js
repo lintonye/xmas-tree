bd01130693eac09784e102c1e86145dcbd4ceecd
Linton Ye <linton@jimulabs.com>
2018-12-14 16:32:42 -0800

    Format code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import Images from './Images';
-import styled, { keyframes } from 'styled-components';
-import _ from 'lodash';
-import Sounds from './Sounds';
-import Sound from './Sound';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Images from "./Images";
+import styled, { keyframes } from "styled-components";
+import _ from "lodash";
+import Sounds from "./Sounds";
+import Sound from "./Sound";
 import { TimelineLite, Power3, Bounce } from "gsap";
-import Preload from './Preload';
-import Spinner from './Spinner';
+import Preload from "./Preload";
+import Spinner from "./Spinner";
 
 const TREE_MAX_AGE = 8;
 const SCENE_WIDTH = 1024;
@@ -18,12 +18,12 @@ const AppDiv = styled.div`
   height: 100vh;
   background: #dddddd;
   display: flex;
-  justify-content: center; 
+  justify-content: center;
   align-items: center;
-`
+`;
 
 const SceneDiv = styled.div`
-  cursor: ${ ({ hideCursor }) => hideCursor ? 'none' : 'default'};
+  cursor: ${({ hideCursor }) => (hideCursor ? "none" : "default")};
   position: relative; /* Need this to make its children's locations relative to it */
   width: ${SCENE_WIDTH}px;
   height: ${SCENE_HEIGHT}px;
@@ -31,35 +31,40 @@ const SceneDiv = styled.div`
   overflow: hidden;
   transform: scale(${props => props.scale});
   flex-shrink: 0; /* Prevent the parent flexbox from cutting off the scene when scaled */
-  -webkit-tap-highlight-color: rgba(0,0,0,0); /* Remove Gray Highlight When Tapping Links in Mobile Safari */
+  -webkit-tap-highlight-color: rgba(
+    0,
+    0,
+    0,
+    0
+  ); /* Remove Gray Highlight When Tapping Links in Mobile Safari */
 `;
 
 const Img = styled.img`
   /* Needed to prevent images from hijacking mouse events from its parent div */
-  pointer-events: none; 
+  pointer-events: none;
   position: absolute;
 `;
 
 const WaterpotImg = Img.extend`
   left: ${props => props.x}px;
   top: ${props => props.y}px;
-  transform: rotate(${({ rotate }) => rotate ? 45 : 0}deg);
+  transform: rotate(${({ rotate }) => (rotate ? 45 : 0)}deg);
   transition: transform 0.3s;
-`
+`;
 
 const WaterImg = Img.extend`
-  transform: translate(${ props => props.x + 100}px, ${props => props.y + 55}px);
-`
+  transform: translate(${props => props.x + 100}px, ${props => props.y + 55}px);
+`;
 
 const SupermanImg = Img.extend`
   left: 150px;
   top: 500px;
-`
+`;
 
 const TreeImg = Img.extend`
   left: 300px;
   top: 150px;
-`
+`;
 
 const BackgroundImg = Img.extend`
   left: -40px;
@@ -70,7 +75,7 @@ const BackgroundImg = Img.extend`
 const ForegroundImg = BackgroundImg.extend`
   left: -250px;
   top: 550px;
- `;
+`;
 
 const MidgroundImg = Img.extend`
   left: -30px;
@@ -91,23 +96,23 @@ const GiftImg = Img.extend`
   top: 490px;
   cursor: pointer;
   pointer-events: auto;
-`
+`;
 
 const OpenGiftImg = Img.extend`
   left: 230px;
   top: 420px;
-`
+`;
 
 const BoxLidImg = Img.extend`
   opacity: 0;
   left: 425px;
   top: 570px;
-`
+`;
 
 const BoxBodyImg = BoxLidImg.extend`
   left: 429px;
   top: 610px;
-`
+`;
 
 const CouponDiv = styled.div`
   background: rgba(0, 0, 0, 0);
@@ -186,17 +191,21 @@ const CouponDiv = styled.div`
   }
 `;
 
-const Background = props => <BackgroundImg src={Images.background} {...props} />
+const Background = props => (
+  <BackgroundImg src={Images.background} {...props} />
+);
 
-const Foreground = props => <ForegroundImg src={Images.foreground} {...props} />
+const Foreground = props => (
+  <ForegroundImg src={Images.foreground} {...props} />
+);
 
-const Waterpot = (props) => {
+const Waterpot = props => {
   const img = Images.waterpot;
   return <WaterpotImg src={img} {...props} />;
 };
 
 class Tree extends Component {
-  state = { growing: false }
+  state = { growing: false };
   componentWillReceiveProps(nextProps) {
     const growing = this.props.age < nextProps.age;
     this.setState({ growing });
@@ -215,29 +224,36 @@ class Tree extends Component {
 const Superman = ({ status }) => {
   let img = Images.supermanBored;
   switch (status) {
-    case 'bored': img = Images.supermanBored; break;
-    case 'magic': img = Images.supermanMagic; break;
-    case 'celebrate': img = Images.supermanCelebrate; break;
-    default: img = Images.supermanBored;
+    case "bored":
+      img = Images.supermanBored;
+      break;
+    case "magic":
+      img = Images.supermanMagic;
+      break;
+    case "celebrate":
+      img = Images.supermanCelebrate;
+      break;
+    default:
+      img = Images.supermanBored;
   }
   return <SupermanImg src={img} />;
-}
+};
 
 const Water = props => (
   <div>
     <WaterImg src={Images.water} {...props} />
     <Sound url={Sounds.flowerWatering} loop={true} />
   </div>
-)
+);
 
 const SocialIcon = ({ name, link }) => (
   <a href={link}>
-    <i className={`fa fa-${name}`} aria-hidden="true"></i>
+    <i className={`fa fa-${name}`} aria-hidden="true" />
   </a>
-)
+);
 
-const Coupon = (props) => (
-  <CouponDiv id='coupon-container' {...props}>
+const Coupon = props => (
+  <CouponDiv id="coupon-container" {...props}>
     <div id="coupon">
       <div id="title-container">
         <h2>React 101 For Designers</h2>
@@ -256,14 +272,23 @@ const Coupon = (props) => (
       <div id="links">
         <a href="http://learnreact.design">learnreact.design</a>
         <div>
-          <SocialIcon name='twitter' link="https://twitter.com/home?status=Happy%20holidays!%20Let's%20grow%20a%20Xmas%20tree%3A%20https%3A//lintonye.github.io/xmas-tree/%20" />
-          <SocialIcon name='facebook' link='https://www.facebook.com/sharer/sharer.php?u=https%3A//lintonye.github.io/xmas-tree/' />
-          <SocialIcon name='google-plus' link='https://plus.google.com/share?url=https%3A//lintonye.github.io/xmas-tree/' />
+          <SocialIcon
+            name="twitter"
+            link="https://twitter.com/home?status=Happy%20holidays!%20Let's%20grow%20a%20Xmas%20tree%3A%20https%3A//lintonye.github.io/xmas-tree/%20"
+          />
+          <SocialIcon
+            name="facebook"
+            link="https://www.facebook.com/sharer/sharer.php?u=https%3A//lintonye.github.io/xmas-tree/"
+          />
+          <SocialIcon
+            name="google-plus"
+            link="https://plus.google.com/share?url=https%3A//lintonye.github.io/xmas-tree/"
+          />
         </div>
       </div>
     </div>
   </CouponDiv>
-)
+);
 
 class AnimatedCoupon extends Component {
   animate = () => {
@@ -272,53 +297,74 @@ class AnimatedCoupon extends Component {
       .delay(0.7)
       .to(this.boxLid, 0.1, { opacity: 1 })
       .to(this.boxBody, 0.1, { opacity: 1 })
-      .to(this.couponContainer, 0.5, { background: 'rgba(0, 0, 0, 0.6)' })
-      .to(this.boxLid, 1, { x: 20, y: -100, scale: 4, ease: Power3.easeInOut }, '-=0.5')
-      .to(this.boxBody, 1, { x: 20, y: 50, scale: 4, ease: Power3.easeInOut }, '-=1')
-      .to(this.boxLid, 0.5, { y: -700, ease: Power3.easeIn }, '-=0.5')
+      .to(this.couponContainer, 0.5, { background: "rgba(0, 0, 0, 0.6)" })
+      .to(
+        this.boxLid,
+        1,
+        { x: 20, y: -100, scale: 4, ease: Power3.easeInOut },
+        "-=0.5"
+      )
+      .to(
+        this.boxBody,
+        1,
+        { x: 20, y: 50, scale: 4, ease: Power3.easeInOut },
+        "-=1"
+      )
+      .to(this.boxLid, 0.5, { y: -700, ease: Power3.easeIn }, "-=0.5")
       .from(this.coupon, 0.4, { y: 200, opacity: 0, ease: Power3.easeInOut })
-      .to(this.coupon, 1, { scale: 1, ease: Bounce.easeOut })
-  }
+      .to(this.coupon, 1, { scale: 1, ease: Bounce.easeOut });
+  };
   componentDidMount() {
     const root = ReactDOM.findDOMNode(this);
-    this.couponContainer = root.querySelector('#coupon-container');
-    this.coupon = root.querySelector('#coupon');
-    this.boxLid = root.querySelector('#box-lid');
-    this.boxBody = root.querySelector('#box-body');
+    this.couponContainer = root.querySelector("#coupon-container");
+    this.coupon = root.querySelector("#coupon");
+    this.boxLid = root.querySelector("#box-lid");
+    this.boxBody = root.querySelector("#box-body");
     this.animate();
   }
   render() {
     return (
       <div>
         <Coupon />
-        <BoxBodyImg id='box-body' src={Images.boxBody} />
-        <BoxLidImg id='box-lid' src={Images.boxLid} />
+        <BoxBodyImg id="box-body" src={Images.boxBody} />
+        <BoxLidImg id="box-lid" src={Images.boxLid} />
       </div>
     );
   }
 }
 
-const Gift = ({ onClick, isOpen }) => <GiftImg onClick={onClick} src={isOpen ? Images.giftUnwrapping : Images.gift} />
+const Gift = ({ onClick, isOpen }) => (
+  <GiftImg
+    onClick={onClick}
+    src={isOpen ? Images.giftUnwrapping : Images.gift}
+  />
+);
 
 class Scene extends Component {
   state = {
-    mouseX: 0, mouseY: 0, treeAge: 0, giftOpen: false
-  }
-  _updateXY = (e) => {
-    const bounds = e.target.getBoundingClientRect()
+    mouseX: 0,
+    mouseY: 0,
+    treeAge: 0,
+    giftOpen: false
+  };
+  _updateXY = e => {
+    const bounds = e.target.getBoundingClientRect();
     // mouseX and Y are relative to the SceneDiv
     const mouseX = (e.clientX - bounds.left) / this.props.scale;
     const mouseY = (e.clientY - bounds.top) / this.props.scale;
     // console.log('mx', mouseX, 'my', mouseY, 'cx', e.clientX, 'cy', e.clientY, 'bounds', bounds)
     this.setState({ mouseX, mouseY });
-  }
-  onMouseMove = (e) => {
+  };
+  onMouseMove = e => {
     this._updateXY(e);
-  }
+  };
   componentDidMount() {
     this._checkWateringInterval = setInterval(() => {
       const now = new Date().getTime();
-      const increaseTreeAge = this._isWateringTree() && (now - this._lastTimeStamp) > 2000 && this.state.treeAge < TREE_MAX_AGE;
+      const increaseTreeAge =
+        this._isWateringTree() &&
+        now - this._lastTimeStamp > 2000 &&
+        this.state.treeAge < TREE_MAX_AGE;
       if (!this._isWateringTree()) this._lastTimeStamp = now;
       if (increaseTreeAge) {
         this.setState(prevState => ({ treeAge: prevState.treeAge + 1 }));
@@ -333,7 +379,7 @@ class Scene extends Component {
     const x = this.state.mouseX - this._parallax().mid;
     const y = this.state.mouseY;
     return x >= x1 && x <= x2 && y >= y1 && y <= y2;
-  }
+  };
   _shouldWaterComeOut = () => this._in(200, 500, 800, 700);
   _isWateringTree = () => this._in(320, 500, 380, 600);
   _parallax = () => {
@@ -341,29 +387,56 @@ class Scene extends Component {
     return {
       far: p / 4,
       mid: p,
-      near: p * 4,
-    }
-  }
+      near: p * 4
+    };
+  };
   _handleGiftClick = () => this.setState({ giftOpen: true });
   render() {
     const { treeAge } = this.state;
     const showXmasTree = treeAge === TREE_MAX_AGE;
-    const supermanStatus = showXmasTree ? 'celebrate' : (this._isWateringTree() ? 'magic' : 'bored');
+    const supermanStatus = showXmasTree
+      ? "celebrate"
+      : this._isWateringTree()
+      ? "magic"
+      : "bored";
     const parallax = this._parallax();
     return (
-      <SceneDiv scale={this.props.scale} onMouseMove={_.throttle(this.onMouseMove, 200)} onClick={this._updateXY} hideCursor={!showXmasTree} >
+      <SceneDiv
+        scale={this.props.scale}
+        onMouseMove={_.throttle(this.onMouseMove, 200)}
+        onClick={this._updateXY}
+        hideCursor={!showXmasTree}
+      >
         <Background offsetX={parallax.far} />
         <MidSceneDiv offsetX={parallax.mid}>
           <MidgroundImg src={Images.midground} />
           <Tree age={treeAge} />
           <Superman status={supermanStatus} />
-          {showXmasTree && <Gift onClick={this._handleGiftClick} isOpen={this.state.giftOpen} />}
-          {showXmasTree && !this.state.giftOpen && <OpenGiftImg src={Images.open} />}
+          {showXmasTree && (
+            <Gift
+              onClick={this._handleGiftClick}
+              isOpen={this.state.giftOpen}
+            />
+          )}
+          {showXmasTree && !this.state.giftOpen && (
+            <OpenGiftImg src={Images.open} />
+          )}
         </MidSceneDiv>
-        {this._shouldWaterComeOut() && !showXmasTree && <Water x={this.state.mouseX} y={this.state.mouseY} />}
-        {!showXmasTree && <Waterpot x={this.state.mouseX} y={this.state.mouseY} rotate={this._shouldWaterComeOut()} />}
+        {this._shouldWaterComeOut() && !showXmasTree && (
+          <Water x={this.state.mouseX} y={this.state.mouseY} />
+        )}
+        {!showXmasTree && (
+          <Waterpot
+            x={this.state.mouseX}
+            y={this.state.mouseY}
+            rotate={this._shouldWaterComeOut()}
+          />
+        )}
         <Foreground offsetX={parallax.near} />
-        <Sound url={showXmasTree ? Sounds.xmas : Sounds.background} loop={true} />
+        <Sound
+          url={showXmasTree ? Sounds.xmas : Sounds.background}
+          loop={true}
+        />
         {this.state.giftOpen && <AnimatedCoupon />}
       </SceneDiv>
     );
@@ -379,12 +452,29 @@ class App extends Component {
     window.removeEventListener("resize", this._update);
   }
   render() {
-    const viewportWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-    const viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-    const scale = Math.min(1, viewportWidth / SCENE_WIDTH, viewportHeight / SCENE_HEIGHT);
+    const viewportWidth = Math.max(
+      document.documentElement.clientWidth,
+      window.innerWidth || 0
+    );
+    const viewportHeight = Math.max(
+      document.documentElement.clientHeight,
+      window.innerHeight || 0
+    );
+    const scale = Math.min(
+      1,
+      viewportWidth / SCENE_WIDTH,
+      viewportHeight / SCENE_HEIGHT
+    );
     return (
       <AppDiv>
-        <Preload indicator={<div><Spinner /><span>Loading...</span></div>}>
+        <Preload
+          indicator={
+            <div>
+              <Spinner />
+              <span>Loading...</span>
+            </div>
+          }
+        >
           <Scene scale={scale} />
         </Preload>
       </AppDiv>
